6f8cebb9511a6fefc6858813d1f97fff0115f121
martinn-K <61994027+martinn-K@users.noreply.github.com>
2023-02-20 01:37:34 -0500

    Update AltControl.lua

--- AltControl.lua
+++ AltControl.lua
@@ -1,12 +1,19 @@
+local enabled = false
+if enabled == false then return end
+
 wait(5)
 
 local owner = 255266623
 local alts = {
-    [1] = 2317523580
+    [1] = 2317523580,
+    [2] = 2053170175
 } -- order isn't important unless you plan on needing order.
 
 local ps = game:GetService("Players")
+local RunService = game:GetService("RunService")
 local plr = ps.LocalPlayer
+local character = plr.Character
+local humanoid = character.Humanoid
 
 if owner == plr.UserId then 
     print("User is the owner.")
@@ -16,6 +23,7 @@ end
 print("This user is an alt.")
 
 local myOwner = ps:GetPlayerByUserId(owner)
+local followF;
 
 local commands = {
     jump = function(args) --easy example
@@ -23,7 +31,34 @@ local commands = {
             plr.Character.Humanoid.JumpPower = args[1]  
         end
         plr.Character.Humanoid.Jump = true
-    end --/jump 50
+    end,
+    chat = function(args)
+        if #args > 0 then
+            local newString = string.gsub(args[1], "_", " ")
+            game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(newString,"All")
+        else
+            print("Chat requires 1 Argument.")
+        end
+    end,
+    follow = function(args)
+        local tablePlace = table.find(alts,plr.UserId) -- 1
+        if args[1] == "true" then
+            followF = RunService.Heartbeat:Connect(function(step)
+                local targetPosition = myOwner.Character.HumanoidRootPart.Position
+                humanoid.WalkSpeed = myOwner.Character.Humanoid.WalkSpeed + 5
+                humanoid:MoveTo((myOwner.Character.HumanoidRootPart.CFrame * CFrame.new(0,0,2.5*tablePlace)).Position)
+                character.HumanoidRootPart.CFrame = CFrame.lookAt(
+                    character.HumanoidRootPart.Position,
+                    Vector3.new(targetPosition.X, character.HumanoidRootPart.Position.Y, targetPosition.Z)
+                )
+                wait(0.000001)
+            end)
+        elseif args[1] == "false" then
+            if followF ~= nil then
+                followF:Disconnect()
+            end
+        end
+    end
 }
 
 myOwner.Chatted:Connect(function(cmd) --/jump 1 2
